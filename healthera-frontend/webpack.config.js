904f8f18b9d9113266e4e9d815f35df8d897ecd1
SINGH CHOUDHARY Jitendra <jitendra.singh@soprasteria.com>
2019-06-13 16:52:48 +0530

    front-end added

---
+++ healthera-frontend/webpack.config.js
@@ -0,0 +1,41 @@
+const webpack = require("webpack");
+const htmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+
+process.env.NODE_ENV = "development";
+
+module.exports = {
+  mode: "development",
+  target: "web",
+  entry: "./index.js",
+  devtool: "cheap-module-source-map",
+
+  devServer: {
+    stats: "minimal",
+    overlay: true,
+    historyApiFallback: true,
+    headers: { "Access-Control-Allow-Origin": "*" },
+    https: false,
+    disableHostCheck: true
+  },
+
+  output: {
+    path: path.resolve(__dirname, "build"),
+    filename: "bundle.js",
+    publicPath: "/"
+  },
+
+  plugins: [new htmlWebpackPlugin({ template: "index.html" })],
+
+  module: {
+    rules: [
+      { test: /\.(js|jsx)$/, exclude: "/node_modules/", use: ["babel-loader"] },
+
+      {
+        test: /\.(css|sass)$/,
+        exclude: "/node_modules",
+        use: ["style-loader", "css-loader"]
+      }
+    ]
+  }
+};
